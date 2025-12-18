34cb89114d5f2e99c258117185fea4ba0efce2b3
LGD_Sunday <12055071971@qq.com>
2025-12-18 21:34:12 +0800

    2.11-异步编程与RxJS

--- src/interview/interview.controller.ts
+++ src/interview/interview.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { EventService } from '../common/services/event.service';
 
 @Controller('interview')
-export class InterviewController {}
+export class InterviewController {
+  constructor(private readonly eventService: EventService) {}
+
+  @Sse('stream')
+  stream(): Observable<MessageEvent> {
+    // 测试 SSE 相应的数据
+    setInterval(() => this.eventService.emit('我是相应的内容'), 2000);
+    return this.eventService.getEvents().pipe(
+      map(
+        (data) =>
+          ({
+            data: JSON.stringify({ message: data }),
+          }) as MessageEvent,
+      ),
+    );
+  }
+}
